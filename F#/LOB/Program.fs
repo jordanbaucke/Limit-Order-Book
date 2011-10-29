843e0fcf3e401bf7784e09459cd77b63a5ff5145
Jordan <jordan.baucke@gmail.com>
2011-10-29 14:11:42 -0600

    Adding QuantCup '11 Info - Challenge Winners implementation in C++

--- F#/LOB/Program.fs
+++ F#/LOB/Program.fs
@@ -1,33 +1,63 @@
 ﻿// Learn more about F# at http://fsharp.net
 
-// order types
+
+(* order types *)
 type orderType = 
     | BUY_MRKT = 0
     | BUY_LMT = 1
     | SELL_MRKT = 2
     | SELL_LMT = 3
     
-// order
+(* order *)
 type order( orderType : orderType, 
             timeRecievedGMT : System.DateTime, 
             tickerSymbol : string, 
+            price : double,
             quantity : double
             ) =
-    member this.orderType = orderType
-    member this.timeRecievedGMT = timeRecievedGMT
-    member this.tickerSymbol = tickerSymbol
-    member this.quantity = quantity
+    member public this.orderId = System.Guid.NewGuid() //stamp the order with a new GUID
+    member public this.orderType = orderType
+    member public this.timeRecievedGMT = timeRecievedGMT
+    member public this.tickerSymbol = tickerSymbol
+    member public this.quantity = quantity
+    member public this.price = price
 
-// the entire LOB is a set of [0..n] limit order books for each symbol in the market
+(* the entire LOB is a set of [0..n] limit order books for each symbol in the market *)
 type lmtOrderBook( tickerSymbol : string, 
-                    last : float, 
-                    bid : float, 
-                    ask : float ) =
-    member this.tickerSymbol = tickerSymbol
-    member this.last = last
-    member this.bid = bid
-    member this.ask = ask
+                    last : decimal, 
+                    bid : decimal, 
+                    ask : decimal,
+                    askOrders : order [],
+                    bidOrders : order []) =
+    member public this.tickerSymbol = tickerSymbol
+    member public this.last = last
+    member public this.bid = bid
+    member public this.ask = ask
+    
+    (*  these lists represent orders currently in the book
+     *  the choice to use LinkedLists is based on O(n) inserts & removals
+     *  random access won't be as necessary
+     *)
+    member public this.askOrders = askOrders
+    member public this.bidOrders : bidOrders
+ //   member public this.bidOrders = order LinkedList
+    
+    // add orders to this book, FIFO
+  //  static member this.addOrder(order : order) = 
+//        let AddLast : askOrders -> LinkedListNode<order>
+    
 
+   // let rec remove i l =
+     //   match i, l with
+      //  | 0, x::xs -> xs
+     //   | i, x::xs -> x::remove (i - 1) xs
+     //   | i, [] -> failwith "index out of range"
+
+
+
+    // cancel orders in this book
+  //  static member this.cancelOrder(order : order) =
+        
 (* Async workflows sample (parallel CPU and I/O tasks) *)
  
 (* A very naive prime number detector *)
